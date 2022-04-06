61b9bef92a204e08b1977d4b62ef6bfa2e4bc7f7
Calixto-Wellington <wellington1081@gmail.com>
2022-04-05 21:17:27 -0300

    Requisitos 7 e 7 ok com ajuda da monitoria

--- middlewares/validateSale.js
+++ middlewares/validateSale.js
@@ -1,24 +1,25 @@
-const validateSale = (req, res, next) => {
-  const payload = [...req.body];
-  
-    payload.forEach(({ productId, quantity }) => {
-      if (quantity < 1) {
-        return res.status(422)
-        .json({ message: '"quantity" must be greater than or equal to 1' });
-      }
-      if (!quantity) {
-      return res.status(400)
-      .json({ message: '"productId" is required' });
-      }
-      if (!productId) {
-        return res.status(400)
-        .json({ message: '"productId" is required' });
-      }
-    });
+const validateProductId = (req, res, next) => {
+  const [sale] = req.body;
+  const { productId } = sale;
 
-  next();
+  if (!productId) return res.status(400).json({ message: '"productId" is required' });
+
+  return next();
 };
 
-module.exports = {
-  validateSale,
+const validateQuantity = (req, res, next) => {
+  const [sale] = req.body;
+  const { quantity } = sale;
+
+  if (!quantity && typeof quantity !== 'number') {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
+
+  if (typeof quantity === 'number' && quantity < 1) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+
+  return next();
 };
+
+module.exports = { validateProductId, validateQuantity };
--- routers/sales.js
+++ routers/sales.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const SalesController = require('../controllers/SalesController');
-const { validateSale } = require('../middlewares/validateSale');
+const { validateProductId } = require('../middlewares/validateSale');
+const { validateQuantity } = require('../middlewares/validateSale');
 
 const router = express.Router();
 
 router.get('/:id', SalesController.getSaleById);
 router.get('/', SalesController.getSalesAll);
-router.post('/', validateSale, SalesController.createProductSales);
-router.put('/:id', validateSale, SalesController.updateSales);
+router.post('/', validateProductId, validateQuantity, SalesController.createProductSales);
+router.put('/:id', validateProductId, validateQuantity, SalesController.updateSales);
 
 module.exports = router;
